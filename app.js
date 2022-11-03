b32082219a89ee3df0a4e972aae49592fdd16410
sheriyarqadir <116277563+sheriyarqadir@users.noreply.github.com>
2022-11-03 17:10:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var a = prompt();
+var firstname = prompt();
+var lastname = prompt();
+console.log("firstname");
+console.log("lastname");
+
+var num = prompt("Number")
+if(num >=50 && num <=60){
+    console.log("C")
+}else{
+    console.log("fail")
+}
+var officer = "yes";
+var Assistent = "yes";
+
+if(officer == "yes"){
+    console.log("Docment Submitted to Assistent")
+}else{
+    console.log("wapsi")
+}
+var age = prompt("Enter Your age");
+var StudentCard = true;
+
+if(age > 18){
+    console.log("Student Allow")
+}else if(StudentCard == true){
+    console.log("Student Allow")
+}else{
+    console.log("Student not Allow")
+}
